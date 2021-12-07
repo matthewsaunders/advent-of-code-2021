42792f1bab687a20770d1a3531691cf6719ad3e4
msaun008 <8563678+msaun008@users.noreply.github.com>
2021-12-07 13:45:21 -0500

    Add day 7 solutions, update repo readme

---
+++ 07/02.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func abs(val int) int {
+	if (val < 0) { val = -val }
+	return val
+}
+
+func sumSeries(val int) int {
+	sum := 0
+	for i := 0; i <= val; i++ { sum += i }
+	return sum
+}
+
+func main() {
+	f, err := os.Open("07/input.txt")
+	if err != nil {
+		fmt.Println(err)
+		panic("Error reading input file")
+	}
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+	lines := []string{}
+	for s.Scan() {
+		lines = append(lines, s.Text())
+	}
+
+	strInputs := strings.Split(lines[0], ",")
+	inputs := []int{}
+	for i := 0; i < len(strInputs); i++ {
+		val, _ := strconv.Atoi(strInputs[i])
+		inputs = append(inputs, val)
+	}
+
+	minIn := inputs[0]
+	maxIn := inputs[0]
+
+	for i := 0; i < len(inputs); i++ {
+		if inputs[i] < minIn { minIn = inputs[i] }
+		if inputs[i] > maxIn { maxIn = inputs[i] }
+	}
+
+	fmt.Printf("min: %v\n", minIn)
+	fmt.Printf("max: %v\n", maxIn)
+
+	position := minIn
+	cost := int(^uint(0) >> 1) // max int
+
+	for i := minIn; i < maxIn; i++ {
+		tmpCost := 0
+
+		for j := 0; j < len(inputs); j++ {
+			tmpCost += sumSeries(abs(inputs[j] - i))
+		}
+
+		if (tmpCost < cost) {
+			fmt.Printf("--> new low")
+			cost = tmpCost
+			position = i
+		}
+
+		fmt.Printf("--position: %v\n", i)
+		fmt.Printf("--cost: %v\n", tmpCost)
+	}
+
+	fmt.Printf("position: %v\n", position)
+	fmt.Printf("cost: %v\n", cost)
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# advent-of-code-2021
+# Advent of Code 2021
+
+This repo is my soultions for the [Advent of Code (AoC)](https://adventofcode.com/) coding competition. I chose to use golang for my solutions to learn the language.
